21fff460f04d98aa5f4d2594ad7bd57812d0c685
Shengran Hu <11711011@mail.sustech.edu.cn>
2023-06-11 01:49:36 -0700

    fix indexing error

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,10 +1,12 @@
 // add toggle functionality to abstract and bibtex buttons
 $(document).ready(function() {
-    $('a.abstract').click(function() {
+    $('a.abstract').click(function(event) {
+        event.preventDefault(); // 阻止默认的链接跳转行为
         $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
         $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
     });
-    $('a.bibtex').click(function() {
+    $('a.bibtex').click(function(event) {
+        event.preventDefault(); // 阻止默认的链接跳转行为
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
         $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
     });
